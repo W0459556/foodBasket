a881dedd4abe87e04766d78174220939b7b844b6
Breanna Young <w0459556@campus.nscc.ca>
2024-01-29 12:30:46 -0400

    edits

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Food picnicBasket[] = new Food[3];
         picnicBasket[0] = new Food("carrot", "vegetable");
-        picnicBasket[0] = new Food("tomato", "fruit");
-        picnicBasket[0] = new Food("porkchop", "meat");
+        picnicBasket[1] = new Food("tomato", "fruit");
+        picnicBasket[2] = new Food("porkchop", "meat");
 
         for(int i = 0; i<picnicBasket.length; i++){
             if(picnicBasket[i].foodName == "porkchop"){
@@ -15,11 +15,6 @@ public class Main {
                 picnicBasket[i].eatIt();
             }
         }
-
-        // System.out.println("Better House Party:");
-        // betterHouseParty.printTotalArea();
-        // betterHouseParty.foundTheCat();
-        // System.out.println("");
 
     }
 }
